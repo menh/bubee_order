01db87c1971bfaf58fb1e4bec7c7144f6f3f3dae
menh <872204778@qq.com>
2018-08-08 01:27:09 +0800

    fill shopping

---
+++ pages/shopping/shopping.js
@@ -0,0 +1,14 @@
+Page({
+  data: {
+    school: ['中山大学', '华南师范'],
+    dormitory: ['深思园10号', '深思园9号'],
+    indexSchool: 0,
+    indexDormitory: 0,
+    categoryBar: [
+      { name: '套餐', id: 'taocan' },
+      { name: '外卖', id: 'waimai' },
+      { name: '夜宵', id: 'yexiao' },
+
+    ],
+  }
+})
