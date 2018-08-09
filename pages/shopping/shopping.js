74881e51647b4424b2fdeb16d642f86e6a6ca1ef
menh <872204778@qq.com>
2018-08-10 01:18:26 +0800

    shopping

--- pages/shopping/shopping.js
+++ pages/shopping/shopping.js
@@ -8,7 +8,27 @@ Page({
       { name: '套餐', id: 'taocan' },
       { name: '外卖', id: 'waimai' },
       { name: '夜宵', id: 'yexiao' },
-
     ],
+    packagesFood: [
+      { title: '标题应该这么长', packageFoodImg1: '../../image/img/food.png', packageFoodImg2: '../../image/img/food.png', packageFoodImg3: '../../image/img/food.png',price: 18.8},
+      { title: '标题应该这么长', packageFoodImg1: '../../image/img/food.png', packageFoodImg2: '../../image/img/food.png', packageFoodImg3: '../../image/img/food.png', price: 18.8 },
+    ],
+    foodList: [
+      { title: '标题应该这么长', foodImg: '../../image/img/food.png', price: 18.8 },
+      { title: '标题应该这么长', foodImg: '../../image/img/food.png',  price: 18.8 },
+      { title: '标题应该这么长', foodImg: '../../image/img/food.png', price: 18.8 },
+      { title: '标题应该这么长', foodImg: '../../image/img/food.png', price: 18.8 },
+      { title: '标题应该这么长', foodImg: '../../image/img/food.png', price: 18.8 },
+      { title: '标题应该这么长', foodImg: '../../image/img/food.png', price: 18.8 },
+      { title: '标题应该这么长', foodImg: '../../image/img/food.png', price: 18.8 },
+      { title: '标题应该这么长', foodImg: '../../image/img/food.png', price: 18.8 },
+      { title: '标题应该这么长', foodImg: '../../image/img/food.png', price: 18.8 },
+      { title: '标题应该这么长', foodImg: '../../image/img/food.png', price: 18.8 },
+      { title: '标题应该这么长', foodImg: '../../image/img/food.png', price: 18.8 },
+      { title: '标题应该这么长', foodImg: '../../image/img/food.png', price: 18.8 },
+      { title: '标题应该这么长', foodImg: '../../image/img/food.png', price: 18.8 },
+      { title: '标题应该这么长', foodImg: '../../image/img/food.png', price: 18.8 },
+    ],
+    showCart: false
   }
 })
